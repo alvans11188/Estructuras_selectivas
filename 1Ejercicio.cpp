1035ff74224680dd70d2f2dd162faaecce13a555
Carlostorres010 <ctorresv@unjbg.edu.pe>
2024-06-14 11:07:01 -0500

    Agregado de enunciado al ejercicio y descripcion

--- 1Ejercicio.cpp
+++ 1Ejercicio.cpp
@@ -1,8 +1,14 @@
+/*
+1. Determinar si un alumno aprueba o reprueba un curso, sabiendo que aprobara si su 
+promedio de tres calificaciones es mayor o igual a 70; reprueba en caso contrario.
+*/
+
 #include <iostream>
 using namespace std;
 
 int main (){
 	int c1, c2, c3, t;
+	cout << "Mediante el siguiente programa se determinara si el alumno esta aprobado desaprobado" << endl;
 	cout <<"Ingrese calificacion 1: ";
 	cin >> c1;
 	cout <<"Ingrese calificacion 2: ";
