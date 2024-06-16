79db6e6ee71c0fab7a87d858fa440c7762d9e987
Alexander <achicallag@unjbg.edu.pe>
2024-06-16 15:44:53 -0500

    Cambio de estructura condicional simple a estructura switch condicional multiple

--- 14Ejercicio.cpp
+++ 14Ejercicio.cpp
@@ -7,29 +7,28 @@ es.
 using namespace std;
 int main (){
 	int n;
-	cout << "Ingrese un numero del 1 al 5";
+	cout << "Ingresa un numero del 1 al 5 para determinar que vocal representa: ";
 	cin >> n;
-	if (n==1){
-		cout << "La vocal es a";
-	}else{
-		if (n==2){
-			cout << "la vocal es e";
-		}else{
-			if (n==3){
-				cout << "la vocal es i";
-			}else{
-				if (n==4){
-					cout << "la vocal es o";
-				}else{
-					if (n==5){
-						cout << "la vocal es u";
-					}else{
-						cout << "se ingreso un valor invalido";
-					}
-				}
-			}
-		}
+	switch(n){
+	case 1: 
+			cout << "La volcal es : a"; 
+			break; 
+	case 2: 
+			cout << "La volcal es : e";
+			break; 
+	case 3:
+			cout << "La volcal es : i";
+			break; 
+	case 4: 
+			cout << "La volcal es : o";
+			break; 
+	case 5: 
+			cout << "La volcal es : u";
+			break; 
+	default: 
+			cout << "El numero ingresado es incorrecto";
+		
+		
 	}
-	
 	return 0;
 }
