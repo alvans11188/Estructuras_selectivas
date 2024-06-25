d2312074513d7d7c61669db5838fa230833e43c8
Dante <dalvarezt@unjbg.edu.pe>
2024-06-25 11:19:46 -0500

    Mejora: Se convirtío el switch a una función void.

--- 14Ejercicio.cpp
+++ 14Ejercicio.cpp
@@ -5,30 +5,33 @@ es.
 
 #include <iostream>
 using namespace std;
-int main (){
+
+void vocal( int a ) {
+	switch(a){
+		case 1: 
+			cout << "La volcal es : a"; 
+			break; 
+		case 2: 
+			cout << "La volcal es : e";
+			break; 
+		case 3:
+			cout << "La volcal es : i";
+			break; 
+		case 4: 
+			cout << "La volcal es : o";
+			break; 
+		case 5: 
+			cout << "La volcal es : u";
+			break; 
+		default: 
+			cout << "El numero ingresado es incorrecto";
+	}
+}
+
+int main() {
 	int n;
 	cout << "Ingresa un numero del 1 al 5 para determinar que vocal representa: ";
 	cin >> n;
-	switch(n){
-	case 1: 
-			cout << "La volcal es : a"; 
-			break; 
-	case 2: 
-			cout << "La volcal es : e";
-			break; 
-	case 3:
-			cout << "La volcal es : i";
-			break; 
-	case 4: 
-			cout << "La volcal es : o";
-			break; 
-	case 5: 
-			cout << "La volcal es : u";
-			break; 
-	default: 
-			cout << "El numero ingresado es incorrecto";
-		
-		
-	}
+	vocal( n );
 	return 0;
 }
