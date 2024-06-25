738ec01043a8d9bdb108d710aa71ef44f32f49dd
Dante <dalvarezt@unjbg.edu.pe>
2024-06-25 11:21:01 -0500

    Mejora: Se arreglo la funcion llamada 'funcion', no sacaba promedio, solo sumaba, se agrego una función void para saber si aprobó o no el estudiante.

--- 1Ejercicio.cpp
+++ 1Ejercicio.cpp
@@ -3,34 +3,35 @@
 promedio de tres calificaciones es mayor o igual a 70; reprueba en caso contrario.
 */
 
-int funcion(int x, int y, int z){
-	int t;
-	t = x + y + z;
-	return t;
-}
-
 #include <iostream>
 using namespace std;
 
+double funcion(double x, double y, double z){
+	double t;
+	t = ( x + y + z ) / 3;
+	return t;
+}
+
+void aprobo( double r ) {
+	if ( r >= 70 ) {
+		cout << "El alumno esta aprobado"<< endl;
+	} else {
+		cout <<"El alumno esta desaprobado" << endl;
+	}
+}
+
 int main (){
-	int c1, c2, c3, t, r;
+	double c1, c2, c3, t, r;
 	cout << "Mediante el siguiente programa se determinara si el alumno esta aprobado desaprobado" << endl;
 	cout <<"Ingrese calificacion 1: ", cin >> c1;
 	cout <<"Ingrese calificacion 2: ", cin >> c2;
 	cout <<"Ingrese calificacion 3: ", cin >> c3;
-	if (c1>=0 & c2>=0 & c3 >=0){
-			r=funcion(c1,c2,c3);
-	
-	if (r >= 70){
-		cout << "El alumno esta aprobado"<< endl;
-	} else { 
-		cout <<"El alumno esta desaprobado" << endl;
-	}
-		
-	}else{
+	if (c1 >= 0 & c2 >= 0 & c3 >= 0){
+		r=funcion(c1,c2,c3);
+		aprobo( r );	
+	} else {
 		cout << "Los datos ingresados son invalidos, intente volver a escribir los numeros"<< endl;
 	}
-	
 	return 0;		
 	
 }
